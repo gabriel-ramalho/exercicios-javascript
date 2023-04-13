638a2c96b5b36ba3e8d39ec2de90b81050875d2a
gabriel-ramalho <102493390+gabriel-ramalho@users.noreply.github.com>
2023-04-13 16:46:50 -0300

    16

--- EstruturaSequencial/16.js
+++ EstruturaSequencial/16.js
@@ -2,3 +2,8 @@
 da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados 
 e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades 
 de latas de tinta a serem compradas e o preço total.*/
+let tamanhoArea = parseInt(prompt("Qual tamanho da area a ser pintada?"))
+let quantidadeLitro = tamanhoArea / 3
+let quantidadeLatas = Math.ceil(quantidadeLitro / 18)
+console.log("Você deverá comprar: " + quantidadeLatas + "lata(s) de tinta(s)")
+console.log("Ficará no valor de: " + quantidadeLatas * 80 +" reais")
