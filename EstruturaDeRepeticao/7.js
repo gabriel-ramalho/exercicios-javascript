1e0e7c8ffaf518040ee370a2d916f998ce011ede
gabriel-ramalho <102493390+gabriel-ramalho@users.noreply.github.com>
2023-04-27 21:30:19 -0300

    7

--- EstruturaDeRepeticao/7.js
+++ EstruturaDeRepeticao/7.js
@@ -1,1 +1,9 @@
 //Faça um programa que leia 5 números e informe o maior número.
+let contador = 0
+for (let i = 0; i < 5; i++){
+  let numero = parseInt(prompt("Digite o " + (i+1) + "º numero"))
+  if(numero > contador) {
+    contador = numero
+  }
+}
+console.log(contador)
