1f90c2a6aa90124b8652dafdc7cc1f5120c2cef3
gabriel-ramalho <102493390+gabriel-ramalho@users.noreply.github.com>
2023-05-09 10:59:58 -0300

    21

--- EstruturaDeRepeticao/21.js
+++ EstruturaDeRepeticao/21.js
@@ -1,2 +1,14 @@
 /**Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. 
  * Um número primo é aquele que é divisível somente por ele mesmo e por 1. */
+let numeroInteiro = parseInt(prompt("Digite um número inteiro"))
+let contador = 0
+for (let i = 1 ; i <= numeroInteiro; i++){
+	if(numeroInteiro % i === 0){
+      contador ++
+    }  
+}
+if(contador == 2 || contador == 1){
+	console.log("É primo")
+} else{
+  console.log("Não é primo")
+}
