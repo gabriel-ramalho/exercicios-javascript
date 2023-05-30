11608af7c026194af820425bb092c3252f54cc76
gabriel-ramalho <102493390+gabriel-ramalho@users.noreply.github.com>
2023-05-30 17:42:55 -0300

    5

--- EstruturaDeClasse/5.ts
+++ EstruturaDeClasse/5.ts
@@ -2,3 +2,28 @@
 os seguintes atributos: número da conta, nome do correntista e saldo. Os métodos são os seguintes: 
 alterarNome, depósito e saque; No construtor, saldo é opcional, com valor default zero e os demais
  atributos são obrigatórios. */
+ class CurrentAccont {
+    constructor(
+        private userName : string,
+        private accountNumber : number,
+        private balance : number
+    ) { }
+    changeName (name:string){
+        this.userName = name;
+        return this.userName;
+    }
+    deposit (amountdeposited: number) {
+        this.balance += amountdeposited;
+        return this.balance;
+    }
+    withdraw (withdrawalamount: number) {
+        this.balance -= withdrawalamount;
+        return this.balance;
+    }
+}
+const currentAccont = new CurrentAccont("Gabriel", 5533, 0)
+console.log(currentAccont)
+currentAccont.deposit(90)
+console.log(currentAccont)
+currentAccont.withdraw(40)
+console.log(currentAccont)
