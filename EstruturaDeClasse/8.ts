99b79514961be63eda28ba8c3f6ebdc9d22bd10d
gabriel-ramalho <102493390+gabriel-ramalho@users.noreply.github.com>
2023-06-12 20:02:04 -0300

    8

--- EstruturaDeClasse/8.ts
+++ EstruturaDeClasse/8.ts
@@ -6,15 +6,24 @@ um macaco coma o outro. É possível criar um macaco canibal? */
 class Monkey{
     constructor (
         private name : string,
-        private stomach : number = 100
+        private stomach : string [],
     ){ }
-    eat(){
-
+    eat(food: string){
+        this.stomach.push(food);
     }
     seeBelly(){
-
+        console.log(this.stomach)
     }
     digest(){
-        
+        this.stomach = [];
     }
 }
+const monkey1 = new Monkey("Cezar", [])
+const monkey2 = new Monkey("Czar", [])
+monkey1.eat("banana")
+monkey1.seeBelly()
+monkey1.eat("maçã")
+monkey1.eat("kiwi")
+monkey1.seeBelly()
+monkey1.digest()
+monkey1.seeBelly()
