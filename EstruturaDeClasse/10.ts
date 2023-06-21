9d863810d5ad43fb45b870be865bd845e368a6e7
gabriel-ramalho <102493390+gabriel-ramalho@users.noreply.github.com>
2023-06-21 18:20:06 -0300

    10 bomba de combustivel

---
+++ EstruturaDeClasse/10.ts
@@ -0,0 +1,38 @@
+/*Classe Bomba de Combustível: Faça um programa completo utilizando classes e métodos que:
+
+a)Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
+    1) tipoCombustivel.
+    2) valorLitro
+    3) quantidadeCombustivel
+b)Possua no mínimo esses métodos:
+    1) abastecerPorValor( ) – método onde é informado o valor a ser abastecido e mostra a quantidade de litros que foi colocada no veículo
+    2) abastecerPorLitro( ) – método onde é informado a quantidade em litros de combustível e mostra o valor a ser pago pelo cliente.
+    3) alterarValor( ) – altera o valor do litro do combustível.
+    4) alterarCombustivel( ) – altera o tipo do combustível.
+    5) alterarQuantidadeCombustivel( ) – altera a quantidade de combustível restante na bomba.
+OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba. */
+class FuelPump{
+    constructor (
+    private typeOfFuel : string,
+    private literValue : number,
+    private fuelQuantity : number,
+    ){}
+    supplyValue(supplyValue: number){
+        return supplyValue/this.literValue
+    }
+    supplyLiter(supplyLiter: number){
+        return this.literValue * supplyLiter
+    }
+    changeValue(){
+
+    }
+    changeFuel(){
+
+    }
+    changeFuelQuantity(){
+        
+    }
+};
+const fuelPump = new FuelPump("gaso", 4, 10)
+console.log(fuelPump.supplyValue(40))
+console.log(fuelPump.supplyLiter(10))
